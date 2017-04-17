3b67320e8524e9b83c757406b73ac0684ef2c875
Matthew Horridge <matthew.horridge@stanford.edu>
2017-04-17 16:14:20 -0700

    Added module entry point

---
+++ owlapi-gwt/src/main/java/org/semanticweb/owlapi/gwt/owlapi.java
@@ -0,0 +1,17 @@
+package org.semanticweb.owlapi.gwt;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 17 Apr 2017
+ */
+public class owlapi implements EntryPoint {
+
+    @Override
+    public void onModuleLoad() {
+        GWT.log("Loaded OWL API Module");
+    }
+}
